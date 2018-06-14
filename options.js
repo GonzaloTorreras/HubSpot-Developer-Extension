b3095130c61353cfa236c93ec5e92a2e5eedb999
Gonzalo Torreras <GonzaloTorreras@users.noreply.github.com>
2018-06-14 15:58:25 +0100

    Older code
    Need to be integrated in the new extension.
    - Hot reload: auto reload the extension after local files changes.
    - Option pages & script: handles an option that is saved and sync. through your sessions.
    - Injected the CSS in the manifest: need to be changed for another method using the new var in the option page.

---
+++ options.js
@@ -0,0 +1,28 @@
+// Saves options to chrome.storage
+function save_options() {
+
+  var darkthemeVal = document.getElementById('darktheme').checked;
+  chrome.storage.sync.set({
+    darktheme: darkthemeVal,
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  chrome.storage.sync.get({
+    darktheme: true
+  }, function(items) {
+    document.getElementById('darktheme').checked = items.darktheme;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
