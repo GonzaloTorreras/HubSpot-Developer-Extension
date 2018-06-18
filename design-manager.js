1a959206fab79a04acab31ef17f8e033e727e4a8
Jon McLaren <jon.m@spingroup.com>
2018-06-18 18:53:39 -0500

    removing console log used for testing

--- design-manager.js
+++ design-manager.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	/*This script runs once the design manager page loads.*/
-	console.log("Yay script is working");
+	
 	chrome.storage.sync.get({
 	    darktheme: true
 	  }, function(items) {
