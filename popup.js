5c6ba2800ab9b35640b94b302d9442f3b2143f66
Jon McLaren <jon.m@spingroup.com>
2018-07-15 11:52:07 -0500

    Google Analytics code added. Currently will not track any button clicks or anything, just opens of the extension popup

--- popup.js
+++ popup.js
@@ -1,4 +1,16 @@
+/*Google Analytics*/
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-122315369-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+/*end google analytics*/
 var developerTools = {
+
 
 	debugReload: function(debugParam) {
 
