c825fff2fc0d2cfbfb5be9fc0325f563488769c4
Jon McLaren <jon.m@spingroup.com>
2018-07-18 20:23:37 -0500

    added temporary height fix to pop-up

--- popup.js
+++ popup.js
@@ -152,6 +152,14 @@ var developerTools = {
 
 	},
 	onLoad: function() {
+		/* Temporary fix to the height bug in the popup. This should get removed soon. */
+		window.setTimeout(() => {
+			$('html, body').css({
+			height: $('.c-tab-slider').outerHeight()
+			});
+		}, 100);
+		/* end temporary bug fix */
+		
 		developerTools.setMenuContext();
 		developerTools.getSettings();
 		/*document.addEventListener('DOMContentLoaded', developerTools.getSettings());
