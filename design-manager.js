0da6d86b8938c386d06965566375ac5ccda8d9ea
Jon McLaren <jon.m@spingroup.com>
2018-12-22 21:59:26 -0600

    stripped more whitespace

--- design-manager.js
+++ design-manager.js
@@ -158,9 +158,7 @@ $(document).ready(function() {
                     html += generateDevMenuItem('Content Settings', hubId, 'https://app.hubspot.com/settings/_HUB_ID_/website/pages/all-domains/page-templates');
                     html += generateDevMenuItem('URL Mappings', hubId, 'https://app.hubspot.com/content/_HUB_ID_/settings/url-mappings');
                     html += generateDevMenuItem('Marketplace', hubId, 'https://app.hubspot.com/marketplace/_HUB_ID_/products');
-
                     return html;
-
                 }
 
                 function generateAppUrl(path) {
