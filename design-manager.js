22f7e58c5d4edd61293f483297a32f43f606229a
Jon McLaren <jon.m@spingroup.com>
2019-03-29 16:21:51 -0500

    Added sprocky here to quote removed emojii, wasnt loading

--- design-manager.js
+++ design-manager.js
@@ -106,7 +106,7 @@ $(document).ready(function() {
                 "Pro tip: in the copyright text of your site you can use {{year}} to always have it say the current year.",
                 "I met Siri once... thought she was basic.",
                 "The HTML module still exists, the HubSpot team just hid it in the marketplace. It is free so you can still use it.",
-                "I see what everyone is saying about me in the Developer Slack... ??",
+                "Sprocky here. I see what everyone is saying about me in the Developer Slack... ",
                 "This just in, HubSpot&apos;s branding team says please refrain from putting googly eyes on the Sprocket Logo.",
                 "Expected Clippy? He was taken by the Snap. #ThanosWinsInEndGame",
                 "Did you know there is a HubL Menu function now that you can use to generate custom menu HTML?",
