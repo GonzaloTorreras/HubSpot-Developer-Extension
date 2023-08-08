81f5c38b545089c2ba105ef207ae4b163af24d26
Bradley Haveman <bradley@haveman.ca>
2023-08-08 13:18:16 -0400

    Firefox support and manifest v3
    Firefox user manifest v2

--- background.js
+++ background.js
@@ -1,97 +1,70 @@
-/*Google Analytics*/
-var _gaq = _gaq || [];
-_gaq.push(["_setAccount", "UA-122315369-1"]);
+// background.js (or your main script where the code runs continuously)
+function injectScript(tab) {
+	if (typeof browser !== 'undefined' && browser.tabs.executeScript) {
+		browser.tabs.executeScript(tab.id, { file: 'design-manager.js' });
+	} else if (typeof chrome !== 'undefined' && chrome.tabs.executeScript) {
+		chrome.tabs.executeScript(tab.id, { file: 'design-manager.js' });
+	}
+}
 
-(function() {
-    var ga = document.createElement("script");
-    ga.type = "text/javascript";
-    ga.async = true;
-    ga.src = "https://ssl.google-analytics.com/ga.js";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(ga, s);
-})();
-/*end google analytics*/
-function trackClick(eventName) {
-    console.log("track:" + eventName);
-    _gaq.push(["_trackEvent", eventName, "clicked"]);
-};
+function handleTabUpdate(tabId, changeInfo, tab) {
+	if (changeInfo.status === 'complete') {
+		injectScript(tab);
+	}
+}
 
-function trackPageView() {
-    _gaq.push(["_trackPageview"]);
-};
+if (typeof browser !== 'undefined' && browser.tabs.onUpdated) {
+	browser.tabs.onUpdated.addListener(handleTabUpdate);
+	browser.runtime.onInstalled.addListener(function () {
+		browser.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
+			const activeTab = tabs[0];
+			injectScript(activeTab);
+		});
+	});
+} else if (typeof chrome !== 'undefined' && chrome.tabs.onUpdated) {
+	chrome.tabs.onUpdated.addListener(handleTabUpdate);
+	chrome.runtime.onInstalled.addListener(function () {
+		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+			const activeTab = tabs[0];
+			injectScript(activeTab);
+		});
+	});
+}
 
-/*listen for clicks*/
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        /*console.log(sender.tab ?
-                    "from a content script:" + sender.tab.url :
-                    "from the extension");*/
+function updateTabUrl(url, params, tabId) {
+	let updatedUrl = url;
 
-        if (request.trackClick) {
-            trackClick(request.trackClick);
-            sendResponse({ farewell: "Tracked!" });
-        }
-    }
-);
+	for (const [key, value] of params.entries()) {
+		const updatedParam = key === 'hsCacheBuster' ? getRandomNumber() : value;
+		updatedUrl.searchParams.set(key, updatedParam);
+	}
 
-/*
-chrome.runtime.onConnect.addListener(function(devToolsConnection) {
-    // assign the listener function to a variable so we can remove it later
-    var devToolsListener = function(message, sender, sendResponse) {
-        // Inject a content script into the identified tab
-        console.log("script:",message.scriptToInject);
-        chrome.tabs.executeScript(message.tabId,
-            { file: message.scriptToInject });
-    }
-    // add the listener
-    devToolsConnection.onMessage.addListener(devToolsListener);
+	chrome.tabs.update(tabId, { url: updatedUrl.toString() });
+}
 
-    devToolsConnection.onDisconnect.addListener(function() {
-         devToolsConnection.onMessage.removeListener(devToolsListener);
-    });
+chrome.commands.onCommand.addListener(function (command) {
+	console.log('Command:', command);
+
+	if (command === 'bust-cache' || command === 'hs-debug') {
+		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+			const tabUrl = new URL(tabs[0].url);
+			const params = tabUrl.searchParams;
+
+			if (command === 'bust-cache') {
+				params.set('hsCacheBuster', getRandomNumber());
+			} else if (command === 'hs-debug') {
+				if (params.has('hsDebug')) {
+					params.delete('hsDebug');
+				} else {
+					params.set('hsDebug', 'True');
+				}
+			}
+
+			updateTabUrl(tabUrl, params, tabs[0].id);
+		});
+	}
 });
-*/
 
-
-chrome.commands.onCommand.addListener(function(command) {
-    console.log("Command:", command);
-    if (command === "bust-cache") {
-        console.log("Cache bustato");
-        _gaq.push(["_trackEvent", "hsCacheBuster", "kbShortcutUsed"]);
-
-
-
-
-
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            var tabUrl = new URL(tabs[0].url);
-            var params = new URLSearchParams(tabUrl.search);
-            var randomNum = Math.floor(Math.random() * 9999) + 1;
-            params.set("hsCacheBuster", randomNum);
-
-
-            chrome.tabs.update(tabs[0].id, { url: tabUrl.origin + tabUrl.pathname + '?' + params.toString() });
-            //window.close();
-
-        });
-    } else if (command === "hs-debug") {
-        console.log("debug activated");
-        _gaq.push(["_trackEvent", "hsDebug", "kbShortcutUsed"]);
-
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            var tabUrl = new URL(tabs[0].url);
-            var params = new URLSearchParams(tabUrl.search);
-
-
-            if (params.has("hsDebug")) {
-                params.delete("hsDebug");
-            } else {
-                params.append("hsDebug", "True");
-            }
-            chrome.tabs.update(tabs[0].id, { url: tabUrl.origin + tabUrl.pathname + '?' + params.toString() });
-
-
-        });
-
-    }
-});
+function getRandomNumber() {
+	return String(Math.floor(Math.random() * 9999) + 1);
+}
